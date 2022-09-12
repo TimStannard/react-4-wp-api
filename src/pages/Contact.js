74f3e4b7c24c73c7a25d6bd793fdd11d0c3bad7e
Tim Stannard <stannard.tim@gmail.com>
2022-09-13 09:57:25 +1200

    contact form working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,30 +4,68 @@ import axios from 'axios';
 const formEndpoint = process.env.REACT_APP_WP_API_FORM_ENDPOINT;
 
 const ContactForm = () => {
+
+    // let testSubmission = new FormData();
+    // testSubmission.append('your-name', 'Tim')
+    // testSubmission.append('your-email', 'Tim@Tim.com')
+    // testSubmission.append('your-message', 'Test')
+
     // setup state for contact form
     const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
     // handle a submit
     const handleSubmit = event => {
         event.preventDefault()
-        axios.post(formEndpoint, {
-            'your-name': 'Fred',
-            'your-email': 'Flintstone',
-            'your-message': 'Message'
+
+        // Format your body response
+        // const emailBody = {
+        //     "your-name": "test",
+        //     "your-email": "stannard.tim@gmail.com",
+        //     "your-message": "test",
+        // };
+
+        // ----Create a FormData object, and append each field to the object
+        // form data is necessary to meet criteria of post request in contact form 7
+        // it's a default javascript function, not unique to react
+        const testForm = new FormData();
+        testForm.append('your-name', 'Tim')
+        testForm.append('your-email', 'Tim@Tim.com')
+        testForm.append('your-message', 'Test')
+
+        // ----post the form, using axios.post
+        // first argument is the formEndpoint, second is the data
+        axios.post(formEndpoint, testForm, {
+            // include headers to tell the mail service we're sending a form
+            // headers tell the server what to expect
+            // e.g. remember back to when we used headers to send an API key with spotify API
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
         })
             .then(function (response) {
                 console.log(response);
+                // update state to show submitted
+                setSubmitted(true);
             })
             .catch(function (error) {
                 console.log(error);
+                // update state to show error
+                setError(true);
             });
-        // update state to show submitted
-        setSubmitted(true);
     };
     if (submitted) {
         return (
             <>
-                <h2>Thank you!</h2>
+                <h3>Thank you!</h3>
                 <p>We'll be in touch soon.</p>
+            </>
+        );
+    }
+    if (error) {
+        return (
+            <>
+                <h3>Error!</h3>
+                <p>Sorry, we were unable to send your message.</p>
             </>
         );
     }
