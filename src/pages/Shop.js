1e55acfd0091756895188cb288f51ea08ff33f54
Tim Stannard <stannard.tim@gmail.com>
2022-09-15 16:51:23 +1200

    added woocommerce shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-// const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const key = process.env.REACT_APP_WOO_KEY
 const secret = process.env.REACT_APP_WOO_SECRET
@@ -10,43 +10,79 @@ const api = new WooCommerceRestApi({
     url: baseUrl,
     consumerKey: key,
     consumerSecret: secret,
-    version: "wc/v3"
+    version: "wc/v3",
+    axiosConfig: {
+        headers: {}
+    }
 });
 
-console.log(api)
+// console.log(api)
 
-// const getProducts = () => {
-//     // use empty array to indicate expected data
-//     const [products, setProducts] = useState([]);
+const AllProducts = () => {
+    // use empty array to indicate expected data
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
-//     api.get("products")
-//         .then((response) => {
-//             if (response.status === 200) {
-//                 setProducts(response.data);
-//             }
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-//     console.log(products);
-//     // const renderedProducts = products.map((artist, index) => {
-//     //     return (
-//     //         <div className="artist-container item-container" key={index}>
-//     //             <Link className="artists-link" to={`/artist/${artist.id}`} >
-//     //                 <img src={artist._embedded['wp:featuredmedia']['0'].source_url} alt={artist.title.rendered} />
-//     //                 <h4 className="name">{artist.title.rendered}</h4>
-//     //             </Link>
-//     //         </div>
+    // use effect will call something once
+    // An empty dependencies array indicates the useEffect will act as a mount and only executes once.
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
-//     //     )
-//     // })
-// }
+    // the woocommerce API is similar to axios, but needs a useEffect to function
+    // otherwise there will be a loop as the component rerenders every time you change the state
+    // so it fetches - changes state - renders again - etc
+    let fetchProducts = () => {
+        api
+            .get("products")
+            .then((response) => {
+                if (response.status === 200) {
+                    setProducts(response.data);
+                    setLoaded(true);
+                }
+            })
+            .catch((error) => { });
+    };
+    // we will see two console logs, one before the api fetches, 
+    // and another when it's fetched
+
+
+
+    const renderedProducts = products.map((product, index) => {
+        return (
+            <div className="product-container item-container" key={index}>
+                <Link className="product-link" to={`/product/${product.id}`} >
+                    <img src={product.images[0].src} alt={product.name} />
+                    <h4 className="name">{product.name}</h4>
+                </Link>
+            </div>
+
+        )
+    })
+
+    if (loaded === true) {
+        // console.log(products)
+        if (products.length > 0) {
+            return (
+                <>
+                    {renderedProducts}
+                </>
+            )
+        } else {
+            return "No products found"
+        }
+    } else {
+        return "Loading..."
+    }
+}
 
 const Shop = () => {
     return (
-        <div>
-            Shop
-            {/* <getProducts /> */}
+        <div id="shop-page" className="page-container">
+            <h2>Shop</h2>
+            <div id="product-grid" className="grid-container">
+                <AllProducts />
+            </div>
         </div>
     )
 }
